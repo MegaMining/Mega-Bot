b47d5fcf91459b0c59e4de97d9539bac7b49fb21
MegaMining <39436289+MegaMining@users.noreply.github.com>
2018-05-20 08:41:47 +0100

    Update The ACTUAL BOT.py

--- The ACTUAL BOT.py
+++ The ACTUAL BOT.py
@@ -6,23 +6,19 @@ import time
 import os
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = "!")
+client = commands.Bot(command_prefix = ".")
+@client.event
+async def on_ready():
+    print("Thank-You for using Mega Bot")
+    await client.change_presence(game=discord.Game(name="Mega Bot"))
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
+    if message.content.startswith('.hello'):
+        msg = 'Hello {0.author.mention} How Are You Today'.format(message)
+        await client.send_message(message.chanenel, msg)
+    if message.content.startswith('.bye'):
+        msg = 'Goodbye {0.author.mention} Hope To See You Again Soon :wave:'.format (message)
         await client.send_message(message.channel, msg)
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
 
 client.run(os.getenv('TOKEN'))
