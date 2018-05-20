83e3ff40065bf084ab53aecb5df8a4b004e542e1
MegaMining <39436289+MegaMining@users.noreply.github.com>
2018-05-20 08:02:32 +0100

    Add files via upload

---
+++ The ACTUAL BOT.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import asyncio
+import time
+import os
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(os.getenv('TOKEN'))
